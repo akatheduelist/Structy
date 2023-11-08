86f0ed763e60dcf1618684d53c37aff4c92b4586
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-11-08 14:15:02 -0600

    intersection JS Complete

---
+++ 01_strings_and_arrays/07_intersection/intersection.py
@@ -0,0 +1,7 @@
+def intersection(a, b):
+  pass # todo
+
+print(intersection([4,2,1,6], [3,6,9,2,10])) # -> [2,6]
+print(intersection([2,4,6], [4,2])) # -> [2,4]
+print(intersection([4,2,1], [1,2,4,6])) # -> [1,2,4]
+print(intersection([0,1,2], [10,11])) # -> []

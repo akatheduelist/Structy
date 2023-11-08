c52d8100f3abb16e329694e4e712bf2e2c133372
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-11-08 14:36:15 -0600

    intersection PY Complete

--- 01_strings_and_arrays/07_intersection/intersection.py
+++ 01_strings_and_arrays/07_intersection/intersection.py
@@ -1,5 +1,15 @@
 def intersection(a, b):
-  pass # todo
+    store = set()
+    res = []
+
+    for num in a:
+        store.add(num)
+
+    for num in b:
+        if num in store:
+            res.append(num)
+    
+    return res    
 
 print(intersection([4,2,1,6], [3,6,9,2,10])) # -> [2,6]
 print(intersection([2,4,6], [4,2])) # -> [2,4]

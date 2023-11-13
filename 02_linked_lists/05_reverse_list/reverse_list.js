0db011c48e5206da226044544a89e23d691b0a2a
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-11-13 16:31:29 -0600

    reverse_list JS iterative solution Complete

---
+++ 02_linked_lists/05_reverse_list/reverse_list.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor (val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+// Approach: Iterative
+const reverseList = (head) => {
+    let current = head;
+    let prev = null;
+    while (current !== null) {
+      const next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    return prev.val;
+  };
+// b -> a -> c -> d -> e -> f
+// a -> b -> c -> d -> e -> f
+// c=b  t=b
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+console.log(reverseList(a)) // f -> e -> d -> c -> b -> a
+
+// x -> y
+const x = new Node('x')
+const y = new Node('y')
+
+x.next = y
+
+console.log(reverseList(x)) // y -> x
+
+// p
+const p = new Node('p')
+
+console.log(reverseList(p)) // p
+
+module.exports = {
+  reverseList
+}

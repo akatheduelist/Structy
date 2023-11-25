e125b1a9a00323755a5a7abced152b1eb2fcc944
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-11-25 15:49:30 -0600

    longest_streak PY iterative solution Complete

---
+++ 02_linked_lists/09_longest_streak/longest_streak.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def longest_streak(head):
+  current = head
+  prev = None
+  max_streak = 0
+  current_streak = 0
+
+  while current is not None:
+    if current.val == prev:
+      prev = current.val
+      current_streak += 1
+      if current_streak > max_streak:
+        max_streak = current_streak
+    else:
+      prev = current.val
+      current_streak = 1
+      if current_streak > max_streak:
+        max_streak = current_streak
+    current = current.next
+
+  return max_streak

17dc5311360a8094cc2c07f1da14f33aa793ded8
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-11-27 15:30:10 -0600

    create_linked_list JS iterative solution Complete

---
+++ 02_linked_lists/12_create_linked_list/create_linked_list.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const createLinkedList = (values) => {
+  let newList = new Node(null);
+  let tail = newList
+
+  for(let i = 0; i < values.length; i++) {
+    let newNode = new Node(values[i])
+    tail.next = newNode
+    tail = tail.next
+  }
+
+  return newList.next
+};
+
+module.exports = {
+  createLinkedList,
+};
+
+console.log(createLinkedList(["h", "e", "y"]));
+// h -> e -> y
+console.log(createLinkedList([1, 7, 1, 8]));
+// 1 -> 7 -> 1 -> 8
+console.log(createLinkedList(["a"]));
+// a
+console.log(createLinkedList([]));
+// null

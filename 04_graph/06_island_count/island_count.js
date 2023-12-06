e63bc6fc75928cab8fd5b1da57e164623e926112
Daniel Lewis <daniel.c.lewis@outlook.com>
2023-12-06 12:38:03 -0600

    island_count JS grid solution Complete

--- 04_graph/06_island_count/island_count.js
+++ 04_graph/06_island_count/island_count.js
@@ -1,37 +1,47 @@
-const islandCount = (grid) => {
-  const visited = new Set([])
-  const islands = 0;
+const islandCount = grid => {
+  const visited = new Set()
+  let islands = 0
 
-  for(let row of grid) { // row 0
-    for(let item of row) { // col 0
-      if(item != 'W') {
-        const coordinates = [grid.indexOf(row), row.indexOf(item)]
-        exploreLand(grid, coordinates, visited);
-      }
+  for (let row = 0; row < grid.length; row++) {
+    // console.log(grid[row])
+    for (let col = 0; col < grid[row].length; col++) {
+      // 0 1
+      const x = row
+      const y = col
+      islands += exploreLand(grid, x, y, visited)
     }
   }
-  return visited;
-};
+  return islands
+}
 
-const exploreLand = (grid, start, visited) => {
-  const queue = [start]
+const exploreLand = (grid, x, y, visited) => {
+  // Depth First Recursive call on explore
+  // Remember we are starting search at (0,0) in the top right so we are looking at the graph plane at a 90deg angle. Ex: graph[2][3] is actually x/y coordinate (3, -2) in our visual graph.
+  // explore(x-1, y) left
+  // explore(x+1, y) right
+  // explore(x, y-1) down
+  // explore(x, y+1) up
+  const inbound = x >= 0 && y >= 0 && x < grid.length && y < grid[x].length // 0,1
+  if (!inbound) return 0
+  
+  const currentXY = x + ',' + y // String -> 0,1
+  if (visited.has(currentXY)) return 0
+  visited.add(currentXY) // Visited Add -> 0,1
+  
+  const currentVal = grid[x][y] // String -> W
+  if (currentVal === 'W') return 0
 
-  while(queue.length) {
-    const current = queue.shift();
+  exploreLand(grid, x - 1, y, visited) // -1, 1
+  exploreLand(grid, x + 1, y, visited) // 1, 1
+  exploreLand(grid, x, y - 1, visited) // 0, 0
+  exploreLand(grid, x, y + 1, visited) // 0, 1
 
-    if(!visited.has(current)){
-      // Do Stufff
-      visited.add(current)
-    }
-
-  }
-
-  return queue
-} 
+  return 1
+}
 
 module.exports = {
-  islandCount,
-};
+  islandCount
+}
 
 const grid = [
   ['W', 'L', 'W', 'W', 'W'],
@@ -39,7 +49,7 @@ const grid = [
   ['W', 'W', 'W', 'L', 'W'],
   ['W', 'W', 'L', 'L', 'W'],
   ['L', 'W', 'W', 'L', 'L'],
-  ['L', 'L', 'W', 'W', 'W'],
-];
+  ['L', 'L', 'W', 'W', 'W']
+]
 
-console.log(islandCount(grid)); // -> 3
+console.log(islandCount(grid)) // -> 3

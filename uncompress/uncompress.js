5802d346841061a8cb7a5dbcb5b9bfbd007fefd2
Daniel Lewis <52519668+akatheduelist@users.noreply.github.com>
2024-02-16 10:45:31 -0600

    Solved 'uncompress' (.js)

--- uncompress/uncompress.js
+++ uncompress/uncompress.js
@@ -1,22 +1,29 @@
 const uncompress = (s) => {
   let str = ''
-  let i = 0
-  let j = 0
+  let pointer1 = 0
+  let pointer2 = 0
   
-  while(j < s.length) {
-    if(!isNaN(s[j])) {
-      j++;
+  while(pointer1 < s.length) {
+    if(!isNaN(s[pointer1])) {
+      pointer1++;
     }
     else {
-      const qty = Number(s.slice(i, j));
-      str += s[j].repeat(qty)
-      j++;
-      i = j;
+      const qty = Number(s.slice(pointer2, pointer1));
+      str += s[pointer1].repeat(qty)
+      pointer1++;
+      pointer2 = pointer1;
     } 
   }
   
+  console.log(str)
   return str  
 };
+​
+uncompress("2c3a1t"); // -> 'ccaaat'
+uncompress("4s2b"); // -> 'ssssbb'
+uncompress("2p1o5p"); // -> 'ppoppppp'
+uncompress("3n12e2z"); // -> 'nnneeeeeeeeeeeezz'
+uncompress("127y"); // ->'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
 ​
 module.exports = {
   uncompress,

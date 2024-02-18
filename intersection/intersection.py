0917811d858f50d546e6f756241cf6514abe7bbe
Daniel Lewis <52519668+akatheduelist@users.noreply.github.com>
2024-02-17 21:12:57 -0600

    Update intersection.py

--- intersection/intersection.py
+++ intersection/intersection.py
@@ -1,4 +1,12 @@
 def intersection(a, b):
+  '''
+  --Intersections with Sets--
+  We use sets to find the inteserction of the two arrays because that have O(1) insertion
+  and deletion. Running a single while loop isn't differnt from two for loops that's just
+  how I did it. Then since we can't use the setA.intersection(setB) we spread setA back
+  into an array and run a filter on it to check if setB has this item. Since we cast the items
+  into a hashed data structure we can get better lookup time over Array.includes().
+  ''' 
   store = set()
   res = []
 ​
@@ -9,7 +17,7 @@ def intersection(a, b):
     if num in store:
       res.append(num)
   
-  # print(res)
+  print(res)
   return res    
   
 intersection([4,2,1,6], [3,6,9,2,10]) # -> [2,6]

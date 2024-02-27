962bebd4ebeb92323b2cfa086df8bb6fd830afa1
Daniel Lewis <52519668+akatheduelist@users.noreply.github.com>
2024-02-27 12:47:45 -0500

    Solved 'get node value' (.py)

--- get_node_value/get_node_value.py
+++ get_node_value/get_node_value.py
@@ -1,24 +1,3 @@
-# class Node:
-#   def __init__(self, val):
-#     self.val = val
-#     self.next = None
-​
-# Approach: Iterative - Keep a count while traversing the linked list. If the count matches the index, return the current node's value. Else, we increment the counter. If the index does not match the count and the next node is "None" then we know we have reached the end of the list without finding the falue and we return False.
-def get_node_value(head, index):
-  count = 0
-  current = head
-​
-  while count <= index:
-    if count == index:
-      return current.val
-    else:
-      count += 1
-    if current.next == None:
-      return None
-    else:
-      current = current.next
-​
-# Approach: Recursive - Pass along the index given and decrease by one when passing through the the recursive call. Once the index is 0 we return the value up the stack.   
 # def get_node_value(head, index):
 #     if head is None:
 #        return None
@@ -47,3 +26,35 @@ def get_node_value(head, index):
 # a.next = b
 # b.next = c
 # c.next = d
+​
+# # a -> b -> c -> d
+​
+# get_node_value(a, 3) # 'd'
+# a = Node("a")
+# b = Node("b")
+# c = Node("c")
+# d = Node("d")
+​
+# a.next = b
+# b.next = c
+# c.next = d
+​
+# # a -> b -> c -> d
+​
+# get_node_value(a, 7) # None
+# node1 = Node("banana")
+# node2 = Node("mango")
+​
+# node1.next = node2
+​
+# # banana -> mango
+​
+# get_node_value(node1, 0) # 'banana'
+# node1 = Node("banana")
+# node2 = Node("mango")
+​
+# node1.next = node2
+​
+# # banana -> mango
+​
+# get_node_value(node1, 1) # 'mango'
